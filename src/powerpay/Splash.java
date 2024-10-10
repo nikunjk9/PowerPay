4f8c06664a82fa558a89c1cfb85d92e61d8711eb
nikunjk9 <nikunjkumar6379@gmail.com>
2024-10-10 12:08:07 +0530

    First commit

--- src/powerpay/Splash.java
+++ src/powerpay/Splash.java
@@ -9,6 +9,8 @@ import java.awt.*;
 public class Splash extends JFrame{                                                             // JFrame: Class of swing
     private JProgressBar progressBar;                                                           // JProgressBar is a Swing component that shows progress visually
     private int progress;
+    private JLabel loadingLabel;                                                                // Added for loading text
+    
     
     // Creating Constructor: This should have same name as class name
     Splash(){
@@ -19,16 +21,24 @@ public class Splash extends JFrame{                                             
         Image i2 = i1.getImage().getScaledInstance(1000, 700,Image.SCALE_DEFAULT);               // Fit the image to the frame 
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3); 
-        image.setBounds(0, 0, 800, 530);
+        image.setBounds(0, 0, 800, 530);                                                         // positioning and sizing components in Java Swing
         add(image);                                                                              // Pasting componenent Frame
+      
+        
+        // Loading text label
+        loadingLabel = new JLabel("Loading...");
+        loadingLabel.setBounds(370, 400, 200, 30);
+        loadingLabel.setForeground(Color.WHITE);
+        loadingLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+        image.add(loadingLabel);
         
         
         // Custom progress bar
         progressBar = new JProgressBar() {
             @Override
-            protected void paintComponent(Graphics g) {
-                Graphics2D g2d = (Graphics2D) g;
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            protected void paintComponent(Graphics g) {                                                        // Graphics object as a parameter, which is like a canvas you can draw on
+                Graphics2D g2d = (Graphics2D) g;                                                               // Graphics2D is a more advanced version of Graphics with additional features like better control over geometry, color management, and text layout.
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);      // enables antialiasing, which smooths out jagged edges in graphics
 
                 // Background (gray part)
                 g2d.setColor(new Color(200, 200, 200));
@@ -41,8 +51,9 @@ public class Splash extends JFrame{                                             
             }
         };
         
+        
         progressBar.setBounds(200, 430, 400, 4);                                                // Make it thinner
-        progressBar.setStringPainted(false);                                                    // Remove percentage text
+        progressBar.setStringPainted(true);                                                    // Remove percentage text
         progressBar.setBorderPainted(false);                                                    // Remove border
         progressBar.setBackground(new Color(200, 200, 200));
         progressBar.setForeground(Color.BLACK);
@@ -52,6 +63,7 @@ public class Splash extends JFrame{                                             
         setLocation(360, 170);
         setUndecorated(true);                                                                   // Removes window decorations: You won't see header of the frame
         setVisible(true);                                                                       // By default visibility of the frame is hidden
+        
         
         // Animation and loading simulation
         try {
@@ -71,6 +83,7 @@ public class Splash extends JFrame{                                             
             e.printStackTrace();
         }
     }
+    
     
     // Remove default UI styling
     static {
